a8b3dff1d3fc85123fcef99b3608d57dc89c1233
Jake Coppinger <jake@jakecoppinger.com>
2022-02-01 21:15:39 +1100

    Add more tests for handle join

--- test/handlers.test.ts
+++ test/handlers.test.ts
@@ -42,7 +42,7 @@ describe("secret matrix bot key", () => {
   })
 });
 describe("#handleJoinEvent()", () => {
-  test("when join event is the matrix bot joining the room, ignore", () => {
+  test("when join event is the matrix bot joining the room, ignore", async () => {
     const joinEvent: MatrixJoinEvent = {
       "type": "m.room.member",
       "sender": matrixBotUsername,
@@ -65,7 +65,7 @@ describe("#handleJoinEvent()", () => {
       "event_id": "$ja135013501351"
     }
     const store = createStore(rootReducer)
-    handleJoinEvent(store,
+    await handleJoinEvent(store,
       undefined as unknown as MatrixClient,
       'room-id',
       joinEvent
@@ -79,6 +79,82 @@ Object {
 }
 `);
   });
+  test("when join event is valid and room doesn't exist, ignore", async () => {
+    mockedFetchRSVPMessageId.mockResolvedValueOnce({
+      status: 'success',
+      matrix_room_address: null,
+      event_exists_for_room: false
+    });
+    const joinEvent: MatrixJoinEvent = {
+      "type": "m.room.member",
+      "sender": '@someone:somewhere',
+      "content": {
+        "membership": "join",
+        "displayname": "dev-bot",
+        "avatar_url": null
+      },
+      "origin_server_ts": 1643680492130,
+      "unsigned": {
+      },
+      "event_id": "$ja135013501351"
+    }
+    const store = createStore(rootReducer)
+    await handleJoinEvent(store,
+      undefined as unknown as MatrixClient,
+      'room-id',
+      joinEvent
+    )
+    expect(mockedFetchRSVPMessageId).toBeCalledTimes(1);
+    expect(sendRSVP).toBeCalledTimes(0);
+    expect(store.getState()).toMatchInlineSnapshot(`
+Object {
+  "rsvpMessages": Object {},
+  "rsvpReactions": Object {},
+}
+`);
+  });
+
+  test("when join event is valid and room exists, send invited RSVP", async () => {
+    mockedFetchRSVPMessageId.mockResolvedValueOnce({
+      status: 'success',
+      matrix_room_address: 'room-id',
+      event_exists_for_room: true
+    });
+    const joinEvent: MatrixJoinEvent = {
+      "type": "m.room.member",
+      "sender": '@someone:somewhere',
+      "content": {
+        "membership": "join",
+        "displayname": "dev-bot",
+        "avatar_url": null
+      },
+      "origin_server_ts": 1643680492130,
+      "unsigned": {
+      },
+      "event_id": "$ja135013501351"
+    }
+    const store = createStore(rootReducer)
+    await handleJoinEvent(store,
+      undefined as unknown as MatrixClient,
+      'room-id',
+      joinEvent
+    )
+    expect(mockedFetchRSVPMessageId).toBeCalledTimes(1);
+    expect(sendRSVP).toBeCalledTimes(1);
+    expect(store.getState().rsvpReactions).toMatchInlineSnapshot(`
+Object {
+  "room-id": Array [
+    Object {
+      "displayname": undefined,
+      "matrixEventId": "$ja135013501351",
+      "sender": "@someone:somewhere",
+      "status": "invited",
+    },
+  ],
+}
+`);
+  });
+
 });
 describe("#handleReaction()", () => {
   test("when event doesn't exist for valid reaction, ignore", async () => {
@@ -146,145 +222,145 @@ Object {
       { roomId: 'matrix-room-id', rsvpMessageEventId: 'new-rsvp-message-id' });
 
     expect(mockedSendRSVP).toBeCalledWith({
-        roomId: 'matrix-room-id', matrix_username: 'sender',
-        status: 'going', displayname: 'sender displayname'
-      });
-      expect(mockedSendRSVP).toBeCalledTimes(1);
+      roomId: 'matrix-room-id', matrix_username: 'sender',
+      status: 'going', displayname: 'sender displayname'
     });
+    expect(mockedSendRSVP).toBeCalledTimes(1);
+  });
 
-    test("when room exists but reaction isn't valid, ignore", async () => {
-      const event: MatrixReactionEvent = {
-        origin_server_ts: 1234,
-        content: {
-          'm.relates_to': {
-            event_id: 'not-our-rsvp-message-id',
-            key: "woo"
-          }
-        },
-        sender: 'sender',
-        event_id: 'event_id'
-
-      }
-      const store = createStore(rootReducer)
-      await (handleReaction(store, undefined as unknown as MatrixClient))('matrix-room-id', event);
-
-      expect(store.getState().rsvpReactions).toStrictEqual({});
-      // Don't call API when invalid emoji
-      expect(mockedFetchRSVPMessageId).toBeCalledTimes(0);
-      expect(mockedSendRSVP).toBeCalledTimes(0);
-    });
-
-    test("when m.relates_to doesn't exist, ignore", async () => {
-      const event: MatrixReactionEvent = {
-        origin_server_ts: 1234,
-        content: {
-        },
-        sender: 'sender',
-        event_id: 'event_id'
-
-      }
-      const store = createStore(rootReducer)
-      await (handleReaction(store, undefined as unknown as MatrixClient))('matrix-room-id', event);
-
-      expect(store.getState().rsvpReactions).toStrictEqual({});
-      expect(mockedSendRSVP).toBeCalledTimes(0);
-    });
-
-    test("when reaction is for a different message to our RSVP message, ignore", async () => {
-      mockedFetchRSVPMessageId.mockResolvedValueOnce({
-        status: 'success',
-        matrix_room_address: 'our-rsvp-message-id',
-        event_exists_for_room: true
-      });
-      const event: MatrixReactionEvent = {
-        origin_server_ts: 1234,
-        content: {
-          'm.relates_to': {
-            event_id: 'not-our-rsvp-message-id',
-            key: "👍️"
-          }
-        },
-        sender: 'sender',
-        event_id: 'event_id'
-
-      }
-      const initialState = {
-        rsvpMessages: {
-          'matrix-room-id': 'our-rsvp-message-id'
+  test("when room exists but reaction isn't valid, ignore", async () => {
+    const event: MatrixReactionEvent = {
+      origin_server_ts: 1234,
+      content: {
+        'm.relates_to': {
+          event_id: 'not-our-rsvp-message-id',
+          key: "woo"
         }
-      }
-      const store = createStore(rootReducer, initialState)
-      await (handleReaction(store, undefined as unknown as MatrixClient))('matrix-room-id', event);
-      expect(store.getState().rsvpReactions).toStrictEqual({});
-      expect(mockedSendRSVP).toBeCalledTimes(0);
-    });
+      },
+      sender: 'sender',
+      event_id: 'event_id'
 
-    test("when reaction is valid and RSVP message ID is in the store", async () => {
-      mockedGetDisplayname.mockResolvedValueOnce('sender displayname');
-      const event: MatrixReactionEvent = {
-        origin_server_ts: 1234,
-        content: {
-          'm.relates_to': {
-            event_id: 'our-rsvp-message-id',
-            key: "👍️"
-          }
-        },
-        sender: 'sender',
-        event_id: 'event_id'
-      }
-      const initialState = {
-        rsvpMessages: {
-          'matrix-room-id': 'our-rsvp-message-id'
+    }
+    const store = createStore(rootReducer)
+    await (handleReaction(store, undefined as unknown as MatrixClient))('matrix-room-id', event);
+
+    expect(store.getState().rsvpReactions).toStrictEqual({});
+    // Don't call API when invalid emoji
+    expect(mockedFetchRSVPMessageId).toBeCalledTimes(0);
+    expect(mockedSendRSVP).toBeCalledTimes(0);
+  });
+
+  test("when m.relates_to doesn't exist, ignore", async () => {
+    const event: MatrixReactionEvent = {
+      origin_server_ts: 1234,
+      content: {
+      },
+      sender: 'sender',
+      event_id: 'event_id'
+
+    }
+    const store = createStore(rootReducer)
+    await (handleReaction(store, undefined as unknown as MatrixClient))('matrix-room-id', event);
+
+    expect(store.getState().rsvpReactions).toStrictEqual({});
+    expect(mockedSendRSVP).toBeCalledTimes(0);
+  });
+
+  test("when reaction is for a different message to our RSVP message, ignore", async () => {
+    mockedFetchRSVPMessageId.mockResolvedValueOnce({
+      status: 'success',
+      matrix_room_address: 'our-rsvp-message-id',
+      event_exists_for_room: true
+    });
+    const event: MatrixReactionEvent = {
+      origin_server_ts: 1234,
+      content: {
+        'm.relates_to': {
+          event_id: 'not-our-rsvp-message-id',
+          key: "👍️"
         }
+      },
+      sender: 'sender',
+      event_id: 'event_id'
+
+    }
+    const initialState = {
+      rsvpMessages: {
+        'matrix-room-id': 'our-rsvp-message-id'
       }
-      const store = createStore(rootReducer, initialState)
-      await (handleReaction(store, undefined as unknown as MatrixClient))('matrix-room-id', event);
-      expect(store.getState().rsvpReactions).toStrictEqual({
+    }
+    const store = createStore(rootReducer, initialState)
+    await (handleReaction(store, undefined as unknown as MatrixClient))('matrix-room-id', event);
+    expect(store.getState().rsvpReactions).toStrictEqual({});
+    expect(mockedSendRSVP).toBeCalledTimes(0);
+  });
+
+  test("when reaction is valid and RSVP message ID is in the store", async () => {
+    mockedGetDisplayname.mockResolvedValueOnce('sender displayname');
+    const event: MatrixReactionEvent = {
+      origin_server_ts: 1234,
+      content: {
+        'm.relates_to': {
+          event_id: 'our-rsvp-message-id',
+          key: "👍️"
+        }
+      },
+      sender: 'sender',
+      event_id: 'event_id'
+    }
+    const initialState = {
+      rsvpMessages: {
+        'matrix-room-id': 'our-rsvp-message-id'
+      }
+    }
+    const store = createStore(rootReducer, initialState)
+    await (handleReaction(store, undefined as unknown as MatrixClient))('matrix-room-id', event);
+    expect(store.getState().rsvpReactions).toStrictEqual({
+      'matrix-room-id': [
+        {
+          status: 'going',
+          matrixEventId: 'event_id',
+          sender: 'sender',
+          displayname: 'sender displayname'
+        }
+      ]
+    });
+    expect(mockedSendRSVP).toBeCalledWith({
+      roomId: 'matrix-room-id', matrix_username: 'sender', status: 'going', displayname: 'sender displayname'
+    });
+    expect(mockedSendRSVP).toBeCalledTimes(1);
+  });
+  test("when reaction is valid & different and RSVP message ID is in the store, updates RSVP", async () => {
+    mockedGetDisplayname.mockResolvedValueOnce('sender displayname');
+    const event: MatrixReactionEvent = {
+      origin_server_ts: 1234,
+      content: {
+        'm.relates_to': {
+          event_id: 'our-rsvp-message-id',
+          key: "👎️",
+        }
+      },
+      sender: 'sender',
+      event_id: 'event_id'
+    }
+    const initialState = {
+      rsvpMessages: {
+        'matrix-room-id': 'our-rsvp-message-id'
+      },
+      rsvpReactions: {
         'matrix-room-id': [
           {
-            status: 'going',
+            status: 'going' as Status,
             matrixEventId: 'event_id',
             sender: 'sender',
             displayname: 'sender displayname'
           }
         ]
-      });
-      expect(mockedSendRSVP).toBeCalledWith({
-        roomId: 'matrix-room-id', matrix_username: 'sender', status: 'going', displayname: 'sender displayname'
-      });
-      expect(mockedSendRSVP).toBeCalledTimes(1);
-    });
-    test("when reaction is valid & different and RSVP message ID is in the store, updates RSVP", async () => {
-      mockedGetDisplayname.mockResolvedValueOnce('sender displayname');
-      const event: MatrixReactionEvent = {
-        origin_server_ts: 1234,
-        content: {
-          'm.relates_to': {
-            event_id: 'our-rsvp-message-id',
-            key: "👎️",
-          }
-        },
-        sender: 'sender',
-        event_id: 'event_id'
       }
-      const initialState = {
-        rsvpMessages: {
-          'matrix-room-id': 'our-rsvp-message-id'
-        },
-        rsvpReactions: {
-          'matrix-room-id': [
-            {
-              status: 'going' as Status,
-              matrixEventId: 'event_id',
-              sender: 'sender',
-              displayname: 'sender displayname'
-            }
-          ]
-        }
-      }
-      const store = createStore(rootReducer, initialState)
-      await (handleReaction(store, undefined as unknown as MatrixClient))('matrix-room-id', event);
-      expect(store.getState().rsvpReactions).toMatchInlineSnapshot(`
+    }
+    const store = createStore(rootReducer, initialState)
+    await (handleReaction(store, undefined as unknown as MatrixClient))('matrix-room-id', event);
+    expect(store.getState().rsvpReactions).toMatchInlineSnapshot(`
 Object {
   "matrix-room-id": Array [
     Object {
@@ -302,9 +378,9 @@ Object {
   ],
 }
 `);
-      expect(mockedSendRSVP).toBeCalledWith({
-        roomId: 'matrix-room-id', matrix_username: 'sender', status: 'notgoing', displayname: 'sender displayname'
-      });
-      expect(mockedSendRSVP).toBeCalledTimes(1);
+    expect(mockedSendRSVP).toBeCalledWith({
+      roomId: 'matrix-room-id', matrix_username: 'sender', status: 'notgoing', displayname: 'sender displayname'
     });
+    expect(mockedSendRSVP).toBeCalledTimes(1);
   });
+});
